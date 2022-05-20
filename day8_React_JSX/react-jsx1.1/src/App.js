3a886df7f903117f3df2af01671fc9b62e2a2eb8
palmer333 <chanchuen.s94@gmail.com>
2022-05-20 14:39:26 +0700

    add 1.1

---
+++ day8_React_JSX/react-jsx1.1/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.js'
+import Content from './Content.js'
+import Footer from './Footer'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Content />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+       <Header />
+       <Content />
+       <Footer />
+     </div>
+    );
+  }
+}
+
+export default App;
